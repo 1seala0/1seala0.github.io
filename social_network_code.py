3e46209581fe559b04276afd9d18b93aebed0906
Rebekah Jacobsen <rebekahjj@gmail.com>
2017-08-03 15:49:10 -0700

    Update social_network_code.py
    newest code

--- social_network_code.py
+++ social_network_code.py
@@ -8,8 +8,7 @@ class User:
         self.favorites = []
         self.connection = []
         #not too sure if I need the next 2:
-        self.logged_on = False
-        self.logged_off = True
+        self.start_over = True
         #see first comment
         User.empCount += 1
 
@@ -50,17 +49,28 @@ class Network:
 
 def main():
 
-    print("Welcome to the new social network!  Would you like to Log In or Sign Up?")
-    first_choice = input("Please type 'log in' to Log In or 'sign up' to Sign Up: ")
-    my_users = Users()
-    while logged_off:
-        #Check for the correct syntax for strings
-        if first_choice == "log in":
-            logged_on = True
+    start_over = True
+    my_users = Network("The new social network")
+    newly_added_users = User("The new social network users")
+    while start_over:
+        print("Welcome to the new social network!  Would you like to Explore the Users or Sign Up?")
+        first_choice = input("Please type 'explore' to Explore the Users or 'sign up' to Sign Up: ")
+
+        if first_choice == "explore":
+            print("")
+            second_choice = input()
 
         elif first_choice == "sign up":
             new_user_name = input("Please type the user name you want: ")
             if new_user_name not in my_users.username_list:
-                
+                my_users.add_user("new_user_name")
+                print(("Welcome to your new social network, {}!").format(new_user_name))
+            else:
+                print("Sorry, that username is already taken...")
+                see_entire_list = input("If you would like to see to the entire list of usernames, type 'why not': ")
+                if see_entire_list =="why not":
+                    for everybody in ny_users.username_list:
+                        print(("{}").format(everybody))
 
         else:
+            Print("Please only type 'log in' or 'sign up'.")
